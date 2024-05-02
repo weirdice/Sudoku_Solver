108cee968daa0114126b337bef71a9cf59a42570
weird ice <post_benjamin@yahoo.com>
2024-05-02 08:34:08 -0400

    remove data files

--- mod_database.py
+++ mod_database.py
@@ -1,15 +1,36 @@
+import tkinter as tk
+from pathlib import Path
+from tkinter import filedialog
+
 import pandas as pd
 
-# "Sudoku_Solver\data_files\sudoku_data.csv"
-data = pd.read_csv(r"data_files/sudoku_data.csv", dtype=str)
-data = data.drop(columns=["solution", "clues"])
+
+def main() -> None:
+
+    root = tk.Tk()
+    root.withdraw()
+    # start tkinter but hide window
+
+    filepath = filedialog.askopenfilename()
+    data_folder = Path("data_files/")
+    save_file = data_folder / "sudoku_data_updated.csv"
+    # location to save modified data
+
+    root.quit()
+    # close tkinter
+
+    # "Sudoku_Solver\data_files\sudoku_data.csv"
+    data = pd.read_csv(filepath, dtype=str)
+    data = data.drop(columns=["solution", "clues"])
+
+    def clean(value: str) -> str:
+        value = str(value).replace(".", "0")
+        return value
+
+    data["puzzle"] = data["puzzle"].apply(clean)
+    data.to_csv(save_file, index=False)
+    print(data.head())
 
 
-def clean(value: str) -> str:
-    value = str(value).replace(".", "0")
-    return value
-
-
-data["puzzle"] = data["puzzle"].apply(clean)
-data.to_csv(r"data_files/sudoku_data_updated.csv", index=False)
-print(data.head())
+if __name__ == "__main__":
+    main()
