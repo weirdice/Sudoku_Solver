2fa99551c3599d54b0ff777c4564661f830475fa
weird ice <post_benjamin@yahoo.com>
2024-05-02 09:09:48 -0400

    timer and file dialog added

--- mod_database.py
+++ mod_database.py
@@ -1,17 +1,22 @@
+"""
+module transforms data from kaggle into a form usable by main module.
+download data from here: https://www.kaggle.com/datasets/radcliffe/3-million-sudoku-puzzles-with-ratings?resource=download"""
+
 import tkinter as tk
 from pathlib import Path
+from time import time
 from tkinter import filedialog
 
 import pandas as pd
 
 
 def main() -> None:
-
     root = tk.Tk()
     root.withdraw()
     # start tkinter but hide window
 
     filepath = filedialog.askopenfilename()
+    start_time = time()  # start timer after getting filepath
     data_folder = Path("data_files/")
     save_file = data_folder / "sudoku_data_updated.csv"
     # location to save modified data
@@ -20,16 +25,25 @@ def main() -> None:
     # close tkinter
 
     # "Sudoku_Solver\data_files\sudoku_data.csv"
-    data = pd.read_csv(filepath, dtype=str)
-    data = data.drop(columns=["solution", "clues"])
+    try:
+        data = pd.read_csv(filepath, dtype=str)
+        data = data.drop(columns=["solution", "clues"])
 
-    def clean(value: str) -> str:
-        value = str(value).replace(".", "0")
-        return value
+        def clean(value: str) -> str:
+            value = str(value).replace(".", "0")
+            return value
 
-    data["puzzle"] = data["puzzle"].apply(clean)
-    data.to_csv(save_file, index=False)
-    print(data.head())
+        data["puzzle"] = data["puzzle"].apply(clean)
+        data.to_csv(save_file, index=False)
+        total_time = round(time() - start_time, 3)
+        print(
+            f"data updated! \n total time: {total_time} seconds \n example of data follows:"
+        )
+        print(data.head())
+    except ValueError:
+        print(
+            f"Invalid file path received please download data from kaggle: \n https://www.kaggle.com/datasets/radcliffe/3-million-sudoku-puzzles-with-ratings?resource=download"
+        )
 
 
 if __name__ == "__main__":
